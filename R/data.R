7ba007d6583fc2e22aa385b9d1ecd9288081a447
ChristineBlume <christine.blume@sbg.ac.at>
2020-11-10 00:42:06 +0100

    update

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset containing the prices and other attributes of almost 54,000
 #'  diamonds. The variables are as follows:
 #'
-#' @format A text file with 955 rows and 5 variables:
+#' @format A vmrk file (can be read as a text file) with 954 rows and 5 variables:
 #' \describe{
 #'   \item{Type}{Describes what column 'Description' shows}
 #'   \item{Description}{Gives the sleep stage according to AASM criteria. 0 = wake, 1 = N1, 2 = N2, 3 = N3, 5 = W}
